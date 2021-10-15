c206ff825f7d61e2e564b2936dd352fe01060712
Francisco Ortiz <panchosdesign@gmail.com>
2021-10-15 01:50:04 -0700

    modificaciones para IOS

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DINAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
